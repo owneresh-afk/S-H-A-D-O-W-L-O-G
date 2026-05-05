20e03b9ec384446e34c976a8ca5b5f57842160e0
owneresh-afk <owneresh@gmail.com>
2026-05-05 11:19:33 +0530

    Update config.py

--- artifacts/telegram-bot/config.py
+++ artifacts/telegram-bot/config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8055441367:AAEwyVJt0r5xKY48DgvmrOD031OZ-CEtKfQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8055441367:AAHh64lFI00rAY_MA3sVT0kcSOZs3PHQ1Ag")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", "8731647972"))
 DB_PATH = os.environ.get("DB_PATH", "bot_data.db")
 FLASK_PORT = int(os.environ.get("PORT", "8000"))
